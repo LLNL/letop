a2248fc7826491180776abd11909039adf5380c1
salazardetroya <salazardetro1@llnl.gov>
2019-11-07 15:15:15 -0800

    steepest descent

--- examples/test_optimization_problem.py
+++ examples/test_optimization_problem.py
@@ -1,6 +1,5 @@
 from firedrake import (Mesh, SpatialCoordinate, FunctionSpace, interpolate,
                         assemble, CellDiameter, File)
-
 from ufl import sin, pi, dx
 
 from parameters_heat_exch import height, width, inlet_width, dist_center, inlet_depth, shift_center, line_sep
@@ -8,6 +7,7 @@ from parameters_heat_exch import OUTMOUTH1, OUTMOUTH2, INMOUTH1, INMOUTH2, INLET
 
 from optimization_problem import OptimizationProblem
 from regularization_solver import RegularizationSolver
+from steepest_descent import SteepestDescent
 
 import sys
 sys.path.append('..')
@@ -61,52 +61,58 @@ parameters = {
 output_dir = "./test_heat_exchanger/"
 phi_pvd = File(output_dir + "phi_evo.pvd")
 phi_pvd.write(phi)
-ItMax = 21
+ItMax = 100
 Jarr = np.zeros( ItMax )
-while It < ItMax and stop == False:
 
-    J = opti_problem.cost_function_evaluation(phi)
-    Jarr[It] = J
+opti_solver = SteepestDescent(opti_problem, reg_solver, pvd_output=phi_pvd)
 
-    # CFL condition
-    maxv = np.max(phi.vector()[:])
-    dt = 1e0 * alpha * hmin / maxv
-    print("dt: {:.5f}".format(dt))
-    # ------- LINE SEARCH ------------------------------------------
-    if It > 0 and Jarr[It] > Jarr[It-1] and ls < ls_max:
-        ls   += 1
-        alpha *= gamma
-        phi.assign(phi_old)
-        phi.assign(hj_solver.solve(velocity, phi, steps=3, dt=dt))
-        print('Line search iteration : %s' % ls)
-        print('Line search step : %.8f' % alpha)
-        print('Function value        : %.10f' % Jarr[It])
-    else:
-        print('************ ITERATION NUMBER %s' % It)
-        print('Function value        : %.10f' % Jarr[It])
-        #print('Compliance            : %.2f' % )
-        #print('Volume fraction       : %.2f' % (vol/(lx*ly)))
-        # Decrease or increase line search step
-        if ls == ls_max: alpha0 = max(alpha0 * gamma2, 0.1*alpha0_init)
-        if ls == 0:      alpha0 = min(alpha0 / gamma2, 1)
-        # Reset alpha and line search index
-        ls,alpha,It = [0,alpha0, It+1]
+opti_solver.solve(phi, velocity, solver_parameters=parameters)
 
-        dJ = opti_problem.derivative_evaluation(phi)
-        reg_solver.solve(velocity, dJ, solver_parameters=parameters)
 
-        phi_old.assign(phi)
-        phi.assign(hj_solver.solve(velocity, phi, steps=3, dt=dt, solver_parameters=parameters))
-        phi_pvd.write(phi)
-
-        # Reinit the level set function every five iterations.
-        if np.mod(It,5) == 0:
-            Dx = hmin
-            phi.assign(reinit_solver.solve(phi, Dx))
-        #------------ STOPPING CRITERION ---------------------------
-        if It>20 and max(abs(Jarr[It-5:It]-Jarr[It-1]))<2.0e-8*Jarr[It-1]/Nx**2/10:
-            stop = True
-
-from numpy.testing import assert_allclose
-print("Jarr at 20: {0:.10f}".format(Jarr[20]))
-assert_allclose(Jarr[20], -46471.13659, rtol=1e-3, atol=1e-6, err_msg='Optimization broken')
+#while It < ItMax and stop == False:
+#
+#    J = opti_problem.cost_function_evaluation(phi)
+#    Jarr[It] = J
+#
+#    # CFL condition
+#    maxv = np.max(phi.vector()[:])
+#    dt = 1e0 * alpha * hmin / maxv
+#    print("dt: {:.5f}".format(dt))
+#    # ------- LINE SEARCH ------------------------------------------
+#    if It > 0 and Jarr[It] > Jarr[It-1] and ls < ls_max:
+#        ls   += 1
+#        alpha *= gamma
+#        phi.assign(phi_old)
+#        phi.assign(hj_solver.solve(velocity, phi, steps=3, dt=dt))
+#        print('Line search iteration : %s' % ls)
+#        print('Line search step : %.8f' % alpha)
+#        print('Function value        : %.10f' % Jarr[It])
+#    else:
+#        print('************ ITERATION NUMBER %s' % It)
+#        print('Function value        : %.10f' % Jarr[It])
+#        #print('Compliance            : %.2f' % )
+#        #print('Volume fraction       : %.2f' % (vol/(lx*ly)))
+#        # Decrease or increase line search step
+#        if ls == ls_max: alpha0 = max(alpha0 * gamma2, 0.1*alpha0_init)
+#        if ls == 0:      alpha0 = min(alpha0 / gamma2, 1)
+#        # Reset alpha and line search index
+#        ls,alpha,It = [0,alpha0, It+1]
+#
+#        dJ = opti_problem.derivative_evaluation(phi)
+#        reg_solver.solve(velocity, dJ, solver_parameters=parameters)
+#
+#        phi_old.assign(phi)
+#        phi.assign(hj_solver.solve(velocity, phi, steps=3, dt=dt, solver_parameters=parameters))
+#        phi_pvd.write(phi)
+#
+#        # Reinit the level set function every five iterations.
+#        if np.mod(It,5) == 0:
+#            Dx = hmin
+#            phi.assign(reinit_solver.solve(phi, Dx))
+#        #------------ STOPPING CRITERION ---------------------------
+#        if It>20 and max(abs(Jarr[It-5:It]-Jarr[It-1]))<2.0e-8*Jarr[It-1]/Nx**2/10:
+#            stop = True
+#
+#from numpy.testing import assert_allclose
+#print("Jarr at 20: {0:.10f}".format(Jarr[20]))
+#assert_allclose(Jarr[20], -46471.13659, rtol=1e-3, atol=1e-6, err_msg='Optimization broken')
