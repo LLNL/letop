d432f8bde26dc7571032204c47887a42df159ad3
Miguel Salazar <salazardetro1@llnl.gov>
2019-11-26 00:55:49 +0000

    name spaces

--- lestofire/optimization/__init__.py
+++ lestofire/optimization/__init__.py
@@ -2,4 +2,5 @@ from .hamilton_jacobi import HJStabSolver, HJSUPG, HJDG
 from .reinit import SignedDistanceSolver
 from .steepest_descent import SteepestDescent
 from .augmented_lagrangian_term import augmented_lagrangian
+from .augmented_lagrangian_term_float import max_fvalue, augmented_lagrangian_float
 from .augmented_lagrangian_optimization import AugmentedLagrangianOptimization
