c088992d59f4cf9b8f824006042f3f41a2fd939a
salazardetroya <salazardetro1@llnl.gov>
2019-12-30 10:49:21 -0800

    towards 3D

--- examples/heat_exchanger/heat_exchanger_al_3D.py
+++ examples/heat_exchanger/heat_exchanger_al_3D.py
@@ -22,7 +22,7 @@ def main():
     X = SpatialCoordinate(mesh)
     x, y, z = X[0], X[1], X[2]
 
-    phi_expr = (y - line_sep - 0.05)
+    phi_expr = -(y - line_sep - 0.05)
 
     PHI = FunctionSpace(mesh, 'CG', 1)
     phi = interpolate(phi_expr , PHI)
@@ -37,7 +37,7 @@ def main():
     tin1 = Constant(10.0)
     tin2 = Constant(100.0)
 
-    iterative = False
+    iterative = True
     if iterative:
         alphamax = 2.5 * mu / (2e-3)
         fieldsplit_1_mg = {
@@ -60,7 +60,7 @@ def main():
                     "mg_levels_ksp_chebyshev_esteig_steps" : 10,
                     "mg_levels_pc_type" : "sor",
                     "pc_gamg_agg_nsmooths" : 4,
-                    "pc_gamg_threshold" : 0.4,
+                    "pc_gamg_threshold" : 0.8, # 0.4 working before
         }
         stokes_parameters = {
                 "mat_type" : "aij",
@@ -117,6 +117,7 @@ def main():
     P1 = FiniteElement("CG", mesh.ufl_cell(), 1)
     TH = P2*P1
     W = FunctionSpace(mesh, TH)
+    print("DOFs: {}".format(W.dim()))
 
     U = TrialFunction(W)
     u, p = split(U)
@@ -161,16 +162,16 @@ def main():
     solver_stokes1 = LinearVariationalSolver(problem, solver_parameters=stokes_parameters) #, nullspace=nullspace)
     solver_stokes1.solve()
 
+    u1, _ = Control(U2).tape_value().split()
+    u1.rename("Velocity")
+    File("u1.pvd").write(u1)
 
     problem = LinearVariationalProblem(stokes(phi, INMOUTH1), L, U2, bcs=bcs2)
     solver_stokes2 = LinearVariationalSolver(problem, solver_parameters=stokes_parameters)
     solver_stokes2.solve()
-    u1, _ = Control(U2).tape_value().split()
     u2, _ = Control(U1).tape_value().split()
-    u1.rename("Velocity")
     u2.rename("Velocity")
     File("u2.pvd").write(u2)
-    File("u1.pvd").write(u1)
 
     # Convection difussion equation
     ks = Constant(1e0)
@@ -179,6 +180,7 @@ def main():
     T = FunctionSpace(mesh, 'DG', 1)
     t = Function(T, name="Temperature")
     w = TestFunction(T)
+    print("DOFs temperature: {}".format(T.dim()))
 
     # Mesh-related functions
     n = FacetNormal(mesh)
@@ -248,11 +250,11 @@ def main():
     c = Control(s)
     Jhat = LevelSetLagrangian(Jform, c, phi, derivative_cb_pre=deriv_cb, lagrange_multiplier=[4e3, 4e3], penalty_value=[1e1, 1e1], penalty_update=[2.0, 2.0], constraint=[Power1, Power2], method='AL')
     Jhat_v = Jhat(phi)
-    exit()
     print("Initial cost function value {:.5f}".format(Jhat_v))
     print("Power drop 1 {:.5f}".format(Power1))
     print("Power drop 2 {:.5f}".format(Power2))
     dJ = Jhat.derivative()
+    exit()
     Jhat.optimize_tape()
 
     velocity = Function(S)
