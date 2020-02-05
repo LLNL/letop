2b0b829f231930f0df228e070e0b758dbba62d49
Miguel Salazar de Troya <salazardetroya@gmail.com>
2020-02-05 10:35:07 -0800

    cantilever example working

--- examples/cantilever/cantilever.py
+++ examples/cantilever/cantilever.py
@@ -17,32 +17,30 @@ def main():
 
     x, y = SpatialCoordinate(mesh)
     PHI = FunctionSpace(mesh, 'CG', 1)
-    phi_expr = sin(y*pi/2.0)*cos(x*pi/2.0) - Constant(0.8)
+    lx = 2.0
+    ly = 1.0
+    phi_expr = -cos(6.0/lx*pi*x) * cos(4.0*pi*y) - 0.6\
+                + max_value(200.0*(0.01-x**2-(y-ly/2)**2),.0)\
+                + max_value(100.0*(x+y-lx-ly+0.1),.0) + max_value(100.0*(x-y-lx+0.1),.0)
     phi = interpolate(phi_expr , PHI)
     phi.rename("LevelSet")
     File(output_dir + "phi_initial.pvd").write(phi)
 
 
-    lx = 15.0
-    ly = 5.0
-    phi_mat = -cos(6.0/lx*pi*x) * cos(4.0*pi*y) - 0.6\
-                + max_value(200.0*(0.01-x**2-(y-ly/2)**2),.0)\
-                + max_value(100.0*(x+y-lx-ly+0.1),.0) + max_value(100.0*(x-y-lx+0.1),.0)
 
-    mu = Constant(1e2)
-    rho_min = 1e-5
-    beta = Constant(100000.0)
+    rho_min = 1e-3
+    beta = Constant(1000.0)
     def hs(phi, beta):
         return Constant(1.0) / (Constant(1.0) + exp(-beta*phi)) + Constant(rho_min)
 
-    H1 = VectorElement("CG", mesh.ufl_cell(), 2)
+    H1 = VectorElement("CG", mesh.ufl_cell(), 1)
     W = FunctionSpace(mesh, H1)
 
     u = TrialFunction(W)
     v = TestFunction(W)
 
     # Elasticity parameters
-    E, nu = 10.0, 0.3
+    E, nu = 1.0, 0.3
     mu, lmbda = Constant(E/(2*(1 + nu))), Constant(E*nu/((1 + nu)*(1 - 2*nu)))
 
     def epsilon(u):
@@ -52,8 +50,8 @@ def main():
         return 2.0*mu*epsilon(v) + lmbda*tr(epsilon(v))*Identity(2)
 
     f = Constant((0.0, 0.0))
-    a = inner(hs(phi, beta)*sigma(u), grad(v))*dx
-    t = Constant((0.0, -5.0))
+    a = inner(hs(-phi, beta)*sigma(u), nabla_grad(v))*dx
+    t = Constant((0.0, -75.0))
     L = inner(t, v)*ds(2)
 
     bc = DirichletBC(W, Constant((0.0, 0.0)), 1)
@@ -65,10 +63,14 @@ def main():
             }
     u_sol = Function(W)
     solve(a==L, u_sol, bcs=[bc], solver_parameters=parameters)#, nullspace=nullspace)
+    File("u_sol.pvd").write(u_sol)
+    Sigma = TensorFunctionSpace(mesh, 'CG', 1)
+    File("sigma.pvd").write(project(sigma(u_sol), Sigma))
 
-    J = inner(t, u_sol)*ds(2)
+    J = inner(hs(-phi, beta)*sigma(u_sol), epsilon(u_sol))*dx + Constant(1000.0)*hs(-phi, beta)*dx
 
     Jform = assemble(J)
+    print("Compliance: {:2f}".format(Jform))
 
     phi_pvd = File(output_dir + "phi_evolution.pvd")
     def deriv_cb(phi):
@@ -76,24 +78,26 @@ def main():
 
     c = Control(s)
     Jhat = LevelSetLagrangian(Jform, c, phi, derivative_cb_pre=deriv_cb)
+    Jhat.optimize_tape()
     Jhat_v = Jhat(phi)
 
     velocity = Function(S)
     bcs_vel_1 = DirichletBC(S, Constant((0.0, 0.0)), 1)
-    bcs_vel = [bcs_vel_1]
-    reg_solver = RegularizationSolver(S, mesh, beta=1e4, gamma=0.0, dx=dx, bcs=bcs_vel)
+    bcs_vel_2 = DirichletBC(S, Constant((0.0, 0.0)), 2)
+    bcs_vel = [bcs_vel_1, bcs_vel_2]
+    reg_solver = RegularizationSolver(S, mesh, beta=1e0, gamma=1e5, dx=dx, bcs=bcs_vel)
 
     options = {
-             'hmin' : 0.001414,
+             'hmin' : 0.2414,
              'hj_stab': 2.0,
              'dt_scale' : 0.1,
-             'n_hj_steps' : 3,
+             'n_hj_steps' : 1,
              'n_reinit' : 5,
-             'max_iter' : 40
+             'max_iter' : 200
              }
 
     opti_solver = SteepestDescent(Jhat, reg_solver, options=options, pvd_output=phi_pvd)
-    Jarr = opti_solver.solve(phi, velocity, iterative=False, tolerance=1e-4)
+    Jarr = opti_solver.solve(phi, velocity, iterative=False, tolerance=1e-10)
 
 if __name__ == '__main__':
     main()
