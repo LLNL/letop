b15b670ddfc9091b02a567b6b807b9651800788c
miguelito <salazardetroya@gmail.com>
2020-03-31 11:36:06 -0700

    forgot the mesh

--- test/run_examples.sh
+++ test/run_examples.sh
@@ -2,11 +2,18 @@
 
 
 source $1/bin/activate
-for d in ../examples/*/; do
-	if [ "../examples/heat_exchanger/" = "$d" ]; then
+cd ../
+lestofire=$(pwd)
+
+
+for d in $lestofire/examples/*/; do
+	if [ "$lestofire/examples/heat_exchanger/" = "$d" ]; then
+		echo $d
 		cd $d
-		python3 heat_exchanger_al.py > ../../test/output.txt
-		cd ../../test/
+		python3 2D_mesh.py
+		gmsh -2 2D_mesh.geo
+		python3 heat_exchanger_al.py > $lestofire/test/output.txt
+		cd $lestofire/test/
 		python3 check_examples_output.py output.txt heat_exchanger
 	fi
 done
