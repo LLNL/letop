641de0efe8bf365f1e8c2a388eca70e950e15c97
miguelito <salazardetroya@gmail.com>
2020-03-31 13:10:20 -0700

    output to stdout and to file

--- test/run_examples.sh
+++ test/run_examples.sh
@@ -12,8 +12,13 @@ for d in $lestofire/examples/*/; do
 		cd $d
 		python3 2D_mesh.py
 		gmsh -2 2D_mesh.geo
-		python3 heat_exchanger_al.py > $lestofire/test/output.txt
+		python3 heat_exchanger_al.py | tee $lestofire/test/output.txt
 		cd $lestofire/test/
-		python3 check_examples_output.py output.txt heat_exchanger
+		if ! python3 check_examples_output.py output.txt heat_exchanger 2> stderr.txt; then
+			echo "problems"
+			exit 1
+		else
+			echo "no problems"
+		fi
 	fi
 done
