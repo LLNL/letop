9d8fcf338a31243eec17aecaeaa1b226262b4b0a
Miguel <salazardetro1@llnl.gov>
2021-03-25 12:08:20 -0700

    add the options files to the meshes are the same

--- test/run_examples.sh
+++ test/run_examples.sh
@@ -11,7 +11,7 @@ for d in $lestofire/examples/*/; do
 		echo "Running ${d}"
 		cd $d
 		python3 2D_mesh.py
-		gmsh -2 2D_mesh.geo
+		gmsh -2 -option 2D_mesh.msh.opt 2D_mesh.geo
 		python3 heat_exchanger_nls.py --n_iters 12 | tee $lestofire/test/output.txt
 		cd $lestofire/test/
 		if ! python3 check_examples_output.py output.txt heat_exchanger 2> stderr.txt; then
@@ -24,7 +24,7 @@ for d in $lestofire/examples/*/; do
 	if [ "$lestofire/examples/cantilever/" = "$d" ]; then
 		echo "Running ${d}"
 		cd $d
-		gmsh -2 mesh_cantilever.geo
+		gmsh -2 -option mesh_cantilever.msh.opt mesh_cantilever.geo
 		python3 cantilever.py --n_iters 12 | tee $lestofire/test/output.txt
 		cd $lestofire/test/
 		if ! python3 check_examples_output.py output.txt cantilever 2> stderr.txt; then
