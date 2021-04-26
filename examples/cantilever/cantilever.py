1144f1a20881121eca8850b6925360f4bee5b29c
Miguel <salazardetro1@llnl.gov>
2021-04-26 15:25:38 -0700

    no wildcard imports

--- examples/cantilever/cantilever.py
+++ examples/cantilever/cantilever.py
@@ -1,157 +1,179 @@
-from firedrake import *
-from firedrake_adjoint import *
+import firedrake as fd
+import firedrake_adjoint as fda
+from firedrake import (
+    inner,
+    dx,
+    ds,
+    cos,
+    pi,
+    exp,
+    max_value,
+    sym,
+    nabla_grad,
+    tr,
+    Identity,
+)
 
 from lestofire.levelset import LevelSetFunctional, RegularizationSolver
 from lestofire.optimization import HJLocalDG, ReinitSolverDG
 from nullspace_optimizer.lestofire import nlspace_solve_shape, Constraint, InfDimProblem
 
-from pyadjoint import no_annotations
 import argparse
 
-parser = argparse.ArgumentParser(description="Heat exchanger")
-parser.add_argument(
-    "--n_iters",
-    dest="n_iters",
-    type=int,
-    action="store",
-    default=200,
-    help="Number of optimization iterations",
-)
-opts = parser.parse_args()
+
+def compliance_optimization():
+    parser = argparse.ArgumentParser(description="Heat exchanger")
+    parser.add_argument(
+        "--n_iters",
+        dest="n_iters",
+        type=int,
+        action="store",
+        default=200,
+        help="Number of optimization iterations",
+    )
+    opts = parser.parse_args()
+
+    output_dir = "cantilever/"
+
+    m = fd.Mesh("./mesh_cantilever.msh")
+    mesh = fd.MeshHierarchy(m, 1)[-1]
+
+    # Perturb the mesh coordinates. Necessary to calculate shape derivatives
+    S = fd.VectorFunctionSpace(mesh, "CG", 1)
+    s = fd.Function(S, name="deform")
+    mesh.coordinates.assign(mesh.coordinates + s)
+
+    # Initial level set function
+    x, y = fd.SpatialCoordinate(mesh)
+    PHI = fd.FunctionSpace(mesh, "DG", 1)
+    lx = 2.0
+    ly = 1.0
+    phi_expr = (
+        -cos(6.0 / lx * pi * x) * cos(4.0 * pi * y)
+        - 0.6
+        + max_value(200.0 * (0.01 - x ** 2 - (y - ly / 2) ** 2), 0.0)
+        + max_value(100.0 * (x + y - lx - ly + 0.1), 0.0)
+        + max_value(100.0 * (x - y - lx + 0.1), 0.0)
+    )
+    # Avoid recording the operation interpolate into the tape.
+    # Otherwise, the shape derivatives will not be correct
+    with fda.stop_annotating():
+        phi = fd.interpolate(phi_expr, PHI)
+        phi.rename("LevelSet")
+        fd.File(output_dir + "phi_initial.pvd").write(phi)
+
+    # Physics. Elasticity
+    rho_min = 1e-5
+    beta = fd.Constant(200.0)
+
+    def hs(phi, beta):
+        return fd.Constant(1.0) / (fd.Constant(1.0) + exp(-beta * phi)) + fd.Constant(
+            rho_min
+        )
+
+    H1_elem = fd.VectorElement("CG", mesh.ufl_cell(), 1)
+    W = fd.FunctionSpace(mesh, H1_elem)
+
+    u = fd.TrialFunction(W)
+    v = fd.TestFunction(W)
+
+    # Elasticity parameters
+    E, nu = 1.0, 0.3
+    mu, lmbda = fd.Constant(E / (2 * (1 + nu))), fd.Constant(
+        E * nu / ((1 + nu) * (1 - 2 * nu))
+    )
+
+    def epsilon(u):
+        return sym(nabla_grad(u))
+
+    def sigma(v):
+        return 2.0 * mu * epsilon(v) + lmbda * tr(epsilon(v)) * Identity(2)
+
+    a = inner(hs(-phi, beta) * sigma(u), nabla_grad(v)) * dx
+    t = fd.Constant((0.0, -75.0))
+    L = inner(t, v) * ds(2)
+
+    bc = fd.DirichletBC(W, fd.Constant((0.0, 0.0)), 1)
+    parameters = {
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "mat_type": "aij",
+        "ksp_converged_reason": None,
+        "pc_factor_mat_solver_type": "mumps",
+    }
+    u_sol = fd.Function(W)
+    fd.solve(
+        a == L, u_sol, bcs=[bc], solver_parameters=parameters
+    )  # , nullspace=nullspace)
+    with fda.stop_annotating():
+        fd.File("u_sol.pvd").write(u_sol)
+
+    # Cost function: Compliance
+    J = fd.assemble(
+        fd.Constant(1e-2) * inner(hs(-phi, beta) * sigma(u_sol), epsilon(u_sol)) * dx
+    )
+
+    # Constraint: Volume
+    with fda.stop_annotating():
+        total_volume = fd.assemble(fd.Constant(1.0) * dx(domain=mesh))
+    VolPen = fd.assemble(hs(-phi, beta) * dx)
+    # Needed to track the value of the volume
+    VolControl = fda.Control(VolPen)
+    Vval = total_volume / 3.0
+
+    phi_pvd = fd.File("phi_evolution.pvd", target_continuity=fd.H1)
+
+    def deriv_cb(phi):
+        with fda.stop_annotating():
+            phi_pvd.write(phi[0])
+
+    c = fda.Control(s)
+    # Lestofire routines
+    Jhat = LevelSetFunctional(J, c, phi, derivative_cb_pre=deriv_cb)
+    Vhat = LevelSetFunctional(VolPen, c, phi)
+    beta_param = 0.1
+    # Boundary conditions for the shape derivatives.
+    # They must be zero at the boundary conditions.
+    bcs_vel = fd.DirichletBC(S, fd.Constant((0.0, 0.0)), (1, 2))
+    # Regularize the shape derivatives
+    reg_solver = RegularizationSolver(
+        S, mesh, beta=beta_param, gamma=1.0e5, dx=dx, bcs=bcs_vel, output_dir=None
+    )
+    # Solver to ensure the level set is a distance function
+    reinit_solver = ReinitSolverDG(n_steps=10, dt=1e-3)
+    hmin = 0.0001
+    # Hamilton-Jacobi equation to advect the level set
+    hj_solver = HJLocalDG(hmin=hmin)
+    dt = 0.001
+    tol = 1e-5
+
+    # Optimization problem
+    vol_constraint = Constraint(Vhat, Vval, VolControl)
+    problem = InfDimProblem(
+        Jhat, reg_solver, hj_solver, reinit_solver, ineqconstraints=vol_constraint
+    )
+
+    parameters = {
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "mat_type": "aij",
+        "ksp_converged_reason": None,
+        "pc_factor_mat_solver_type": "mumps",
+    }
+
+    params = {
+        "alphaC": 1.0,
+        "K": 0.1,
+        "debug": 5,
+        "alphaJ": 1.0,
+        "dt": dt,
+        "maxtrials": 10,
+        "maxit": opts.n_iters,
+        "itnormalisation": 50,
+        "tol": tol,
+    }
+    results = nlspace_solve_shape(problem, params)
 
 
-output_dir = "cantilever/"
-
-m = Mesh("./mesh_cantilever.msh")
-mesh = MeshHierarchy(m, 1)[-1]
-
-S = VectorFunctionSpace(mesh, "CG", 1)
-s = Function(S, name="deform")
-mesh.coordinates.assign(mesh.coordinates + s)
-
-x, y = SpatialCoordinate(mesh)
-PHI = FunctionSpace(mesh, "DG", 1)
-lx = 2.0
-ly = 1.0
-phi_expr = (
-    -cos(6.0 / lx * pi * x) * cos(4.0 * pi * y)
-    - 0.6
-    + max_value(200.0 * (0.01 - x ** 2 - (y - ly / 2) ** 2), 0.0)
-    + max_value(100.0 * (x + y - lx - ly + 0.1), 0.0)
-    + max_value(100.0 * (x - y - lx + 0.1), 0.0)
-)
-with stop_annotating():
-    phi = interpolate(phi_expr, PHI)
-    phi.rename("LevelSet")
-    File(output_dir + "phi_initial.pvd").write(phi)
-
-
-rho_min = 1e-3
-beta = Constant(200.0)
-
-
-def hs(phi, beta):
-    return Constant(1.0) / (Constant(1.0) + exp(-beta * phi)) + Constant(rho_min)
-
-
-H1_elem = VectorElement("CG", mesh.ufl_cell(), 1)
-W = FunctionSpace(mesh, H1_elem)
-
-u = TrialFunction(W)
-v = TestFunction(W)
-
-# Elasticity parameters
-E, nu = 1.0, 0.3
-mu, lmbda = Constant(E / (2 * (1 + nu))), Constant(E * nu / ((1 + nu) * (1 - 2 * nu)))
-
-
-def epsilon(u):
-    return sym(nabla_grad(u))
-
-
-def sigma(v):
-    return 2.0 * mu * epsilon(v) + lmbda * tr(epsilon(v)) * Identity(2)
-
-
-f = Constant((0.0, 0.0))
-a = inner(hs(-phi, beta) * sigma(u), nabla_grad(v)) * dx
-t = Constant((0.0, -75.0))
-L = inner(t, v) * ds(2)
-
-bc = DirichletBC(W, Constant((0.0, 0.0)), 1)
-parameters = {
-    "ksp_type": "preonly",
-    "pc_type": "lu",
-    "mat_type": "aij",
-    "ksp_converged_reason": None,
-    "pc_factor_mat_solver_type": "mumps",
-}
-u_sol = Function(W)
-solve(a == L, u_sol, bcs=[bc], solver_parameters=parameters)  # , nullspace=nullspace)
-with stop_annotating():
-    File("u_sol.pvd").write(u_sol)
-
-Jform = assemble(
-    Constant(1.0) * inner(hs(-phi, beta) * sigma(u_sol), epsilon(u_sol)) * dx
-)
-
-with stop_annotating():
-    total_volume = assemble(Constant(1.0) * dx(domain=mesh))
-VolPen = assemble(hs(-phi, beta) * dx)
-VolControl = Control(VolPen)
-
-Vval = total_volume / 3.0
-
-
-velocity = Function(S)
-bcs_vel_1 = DirichletBC(S, Constant((0.0, 0.0)), 1)
-bcs_vel_2 = DirichletBC(S, Constant((0.0, 0.0)), 2)
-bcs_vel = [bcs_vel_2]
-
-
-phi_pvd = File("phi_evolution.pvd", target_continuity=H1)
-
-
-def deriv_cb(phi):
-    with stop_annotating():
-        phi_pvd.write(phi[0])
-
-
-c = Control(s)
-Jhat = LevelSetFunctional(Jform, c, phi, derivative_cb_pre=deriv_cb)
-Vhat = LevelSetFunctional(VolPen, c, phi)
-beta_param = 1e0
-reg_solver = RegularizationSolver(
-    S, mesh, beta=beta_param, gamma=1.0e5, dx=dx, bcs=bcs_vel, output_dir=None
-)
-reinit_solver = ReinitSolverDG(n_steps=10, dt=1e-3)
-hmin = 0.0001
-hj_solver = HJLocalDG(hmin=hmin)
-dt = 0.00001
-tol = 1e-5
-
-vol_constraint = Constraint(Vhat, Vval, VolControl)
-problem = InfDimProblem(
-    Jhat, reg_solver, hj_solver, reinit_solver, ineqconstraints=vol_constraint
-)
-
-parameters = {
-    "ksp_type": "preonly",
-    "pc_type": "lu",
-    "mat_type": "aij",
-    "ksp_converged_reason": None,
-    "pc_factor_mat_solver_type": "mumps",
-}
-
-params = {
-    "alphaC": 1.0,
-    "K": 0.1,
-    "debug": 5,
-    "alphaJ": 1.0,
-    "dt": dt,
-    "maxtrials": 10,
-    "maxit": opts.n_iters,
-    "itnormalisation": 50,
-    "tol": tol,
-}
-results = nlspace_solve_shape(problem, params)
+if __name__ == "__main__":
+    compliance_optimization()
--- examples/heat_exchanger/heat_exchanger_nls.py
+++ examples/heat_exchanger/heat_exchanger_nls.py
@@ -1,11 +1,25 @@
-from firedrake import *
-from firedrake_adjoint import *
+import firedrake as fd
+import firedrake_adjoint as fda
+from firedrake import (
+    inner,
+    grad,
+    div,
+    dx,
+    ds,
+    dot,
+    dS,
+    jump,
+    avg,
+    sin,
+    cos,
+    pi,
+    exp,
+)
 
 from lestofire.levelset import LevelSetFunctional, RegularizationSolver
 from lestofire.optimization import HJLocalDG, ReinitSolverDG
 from nullspace_optimizer.lestofire import nlspace_solve_shape, Constraint, InfDimProblem
-
-from params import (
+from .parameters import (
     INMOUTH2,
     INMOUTH1,
     line_sep,
@@ -21,266 +35,275 @@ from params import (
     OUTMOUTH2,
 )
 
-from pyadjoint import no_annotations, stop_annotating
+from pyadjoint import stop_annotating
 import argparse
 
-parser = argparse.ArgumentParser(description="Level set method parameters")
-parser.add_argument(
-    "--mu", action="store", dest="mu", type=float, help="Viscosity", default=0.03
-)
-parser.add_argument(
-    "--n_iters",
-    dest="n_iters",
-    type=int,
-    action="store",
-    default=1000,
-    help="Number of optimization iterations",
-)
-opts = parser.parse_args()
 
-output_dir = "2D/"
+def heat_exchanger_optimization():
 
-mesh = Mesh("./2D_mesh.msh")
-
-S = VectorFunctionSpace(mesh, "CG", 1)
-s = Function(S, name="deform")
-mesh.coordinates.assign(mesh.coordinates + s)
-
-x, y = SpatialCoordinate(mesh)
-PHI = FunctionSpace(mesh, "DG", 1)
-phi_expr = sin(y * pi / 0.2) * cos(x * pi / 0.2) - Constant(0.8)
-with stop_annotating():
-    phi = interpolate(phi_expr, PHI)
-    phi.rename("LevelSet")
-    File(output_dir + "phi_initial.pvd").write(phi)
-
-
-# Parameters
-mu = Constant(opts.mu)  # viscosity
-alphamin = 1e-12
-alphamax = 2.5 / (2e-4)
-parameters = {
-    "mat_type": "aij",
-    "ksp_type": "preonly",
-    "ksp_converged_reason": None,
-    "pc_type": "lu",
-    "pc_factor_mat_solver_type": "mumps",
-}
-stokes_parameters = parameters
-temperature_parameters = parameters
-u_inflow = 2e-3
-tin1 = Constant(10.0)
-tin2 = Constant(100.0)
-
-Re = u_inflow * width / mu.values()[0]
-
-
-P2 = VectorElement("CG", mesh.ufl_cell(), 2)
-P1 = FiniteElement("CG", mesh.ufl_cell(), 1)
-TH = P2 * P1
-W = FunctionSpace(mesh, TH)
-
-U = TrialFunction(W)
-u, p = split(U)
-V = TestFunction(W)
-v, q = split(V)
-
-
-epsilon = Constant(10000.0)
-
-
-def hs(phi, epsilon):
-    return Constant(alphamax) * Constant(1.0) / (
-        Constant(1.0) + exp(-epsilon * phi)
-    ) + Constant(alphamin)
-
-
-def stokes(phi, BLOCK_INLET_MOUTH, BLOCK_OUTLET_MOUTH):
-    a_fluid = mu * inner(grad(u), grad(v)) - div(v) * p - q * div(u)
-    darcy_term = inner(u, v)
-    return (
-        a_fluid * dx
-        + hs(phi, epsilon) * darcy_term * dx(0)
-        + alphamax * darcy_term * (dx(BLOCK_INLET_MOUTH) + dx(BLOCK_OUTLET_MOUTH))
+    parser = argparse.ArgumentParser(description="Level set method parameters")
+    parser.add_argument(
+        "--mu", action="store", dest="mu", type=float, help="Viscosity", default=0.03
+    )
+    parser.add_argument(
+        "--n_iters",
+        dest="n_iters",
+        type=int,
+        action="store",
+        default=1000,
+        help="Number of optimization iterations",
     )
 
+    opts = parser.parse_args()
 
-# Dirichelt boundary conditions
-inflow1 = as_vector(
-    [
-        u_inflow
-        * sin(((y - (line_sep - (dist_center + inlet_width))) * pi) / inlet_width),
-        0.0,
-    ]
-)
-inflow2 = as_vector(
-    [u_inflow * sin(((y - (line_sep + dist_center)) * pi) / inlet_width), 0.0]
-)
+    output_dir = "2D/"
 
-noslip = Constant((0.0, 0.0))
+    # Perturb the mesh coordinates. Necessary to calculate shape derivatives
+    mesh = fd.Mesh("./2D_mesh.msh")
+    S = fd.VectorFunctionSpace(mesh, "CG", 1)
+    s = fd.Function(S, name="deform")
+    mesh.coordinates.assign(mesh.coordinates + s)
 
-# Stokes 1
-bcs1_1 = DirichletBC(W.sub(0), noslip, WALLS)
-bcs1_2 = DirichletBC(W.sub(0), inflow1, INLET1)
-bcs1_3 = DirichletBC(W.sub(1), Constant(0.0), OUTLET1)
-bcs1_4 = DirichletBC(W.sub(0), noslip, INLET2)
-bcs1_5 = DirichletBC(W.sub(0), noslip, OUTLET2)
-bcs1 = [bcs1_1, bcs1_2, bcs1_3, bcs1_4, bcs1_5]
+    # Initial level set function
+    x, y = fd.SpatialCoordinate(mesh)
+    PHI = fd.FunctionSpace(mesh, "DG", 1)
+    phi_expr = sin(y * pi / 0.2) * cos(x * pi / 0.2) - fd.Constant(0.8)
+    # Avoid recording the operation interpolate into the tape.
+    # Otherwise, the shape derivatives will not be correct
+    with fda.stop_annotating():
+        phi = fd.interpolate(phi_expr, PHI)
+        phi.rename("LevelSet")
+        fd.File(output_dir + "phi_initial.pvd").write(phi)
 
-# Stokes 2
-bcs2_1 = DirichletBC(W.sub(0), noslip, WALLS)
-bcs2_2 = DirichletBC(W.sub(0), inflow2, INLET2)
-bcs2_3 = DirichletBC(W.sub(1), Constant(0.0), OUTLET2)
-bcs2_4 = DirichletBC(W.sub(0), noslip, INLET1)
-bcs2_5 = DirichletBC(W.sub(0), noslip, OUTLET1)
-bcs2 = [bcs2_1, bcs2_2, bcs2_3, bcs2_4, bcs2_5]
+    # Physics
+    mu = fd.Constant(opts.mu)  # viscosity
+    alphamin = 1e-12
+    alphamax = 2.5 / (2e-4)
+    parameters = {
+        "mat_type": "aij",
+        "ksp_type": "preonly",
+        "ksp_converged_reason": None,
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "mumps",
+    }
+    stokes_parameters = parameters
+    temperature_parameters = parameters
+    u_inflow = 2e-3
+    tin1 = fd.Constant(10.0)
+    tin2 = fd.Constant(100.0)
 
-# Forward problems
-U1, U2 = Function(W), Function(W)
-L = inner(Constant((0.0, 0.0, 0.0)), V) * dx
-problem = LinearVariationalProblem(stokes(-phi, INMOUTH2, OUTMOUTH2), L, U1, bcs=bcs1)
-nullspace = MixedVectorSpaceBasis(W, [W.sub(0), VectorSpaceBasis(constant=True)])
-solver_stokes1 = LinearVariationalSolver(problem, solver_parameters=stokes_parameters)
-solver_stokes1.solve()
-problem = LinearVariationalProblem(stokes(phi, INMOUTH1, OUTMOUTH1), L, U2, bcs=bcs2)
-solver_stokes2 = LinearVariationalSolver(problem, solver_parameters=stokes_parameters)
-solver_stokes2.solve()
+    P2 = fd.VectorElement("CG", mesh.ufl_cell(), 2)
+    P1 = fd.FiniteElement("CG", mesh.ufl_cell(), 1)
+    TH = P2 * P1
+    W = fd.FunctionSpace(mesh, TH)
 
-# Convection difussion equation
-ks = Constant(1e0)
-cp_value = 5.0e5
-cp = Constant(cp_value)
-T = FunctionSpace(mesh, "DG", 1)
-t = Function(T, name="Temperature")
-w = TestFunction(T)
+    U = fd.TrialFunction(W)
+    u, p = fd.split(U)
+    V = fd.TestFunction(W)
+    v, q = fd.split(V)
 
-# Mesh-related functions
-n = FacetNormal(mesh)
-h = CellDiameter(mesh)
-u1, p1 = split(U1)
-u2, p2 = split(U2)
+    epsilon = fd.Constant(10000.0)
 
+    def hs(phi, epsilon):
+        return fd.Constant(alphamax) * fd.Constant(1.0) / (
+            fd.Constant(1.0) + exp(-epsilon * phi)
+        ) + fd.Constant(alphamin)
 
-def upwind(u):
-    return (dot(u, n) + abs(dot(u, n))) / 2.0
+    def stokes(phi, BLOCK_INLET_MOUTH, BLOCK_OUTLET_MOUTH):
+        a_fluid = mu * inner(grad(u), grad(v)) - div(v) * p - q * div(u)
+        darcy_term = inner(u, v)
+        return (
+            a_fluid * dx
+            + hs(phi, epsilon) * darcy_term * dx(0)
+            + alphamax * darcy_term * (dx(BLOCK_INLET_MOUTH) + dx(BLOCK_OUTLET_MOUTH))
+        )
 
-
-u1n = upwind(u1)
-u2n = upwind(u2)
-
-# Penalty term
-alpha = Constant(500.0)
-# Bilinear form
-a_int = dot(grad(w), ks * grad(t) - cp * (u1 + u2) * t) * dx
-
-a_fac = (
-    Constant(-1.0) * ks * dot(avg(grad(w)), jump(t, n)) * dS
-    + Constant(-1.0) * ks * dot(jump(w, n), avg(grad(t))) * dS
-    + ks("+") * (alpha("+") / avg(h)) * dot(jump(w, n), jump(t, n)) * dS
-)
-
-a_vel = (
-    dot(
-        jump(w),
-        cp * (u1n("+") + u2n("+")) * t("+") - cp * (u1n("-") + u2n("-")) * t("-"),
+    # Dirichlet boundary conditions
+    inflow1 = fd.as_vector(
+        [
+            u_inflow
+            * sin(((y - (line_sep - (dist_center + inlet_width))) * pi) / inlet_width),
+            0.0,
+        ]
     )
-    * dS
-    + dot(w, cp * (u1n + u2n) * t) * ds
-)
+    inflow2 = fd.as_vector(
+        [u_inflow * sin(((y - (line_sep + dist_center)) * pi) / inlet_width), 0.0]
+    )
 
-a_bnd = (
-    dot(w, cp * dot(u1 + u2, n) * t) * (ds(INLET1) + ds(INLET2))
-    + w * t * (ds(INLET1) + ds(INLET2))
-    - w * tin1 * ds(INLET1)
-    - w * tin2 * ds(INLET2)
-    + alpha / h * ks * w * t * (ds(INLET1) + ds(INLET2))
-    - ks * dot(grad(w), t * n) * (ds(INLET1) + ds(INLET2))
-    - ks * dot(grad(t), w * n) * (ds(INLET1) + ds(INLET2))
-)
+    noslip = fd.Constant((0.0, 0.0))
 
-aT = a_int + a_fac + a_vel + a_bnd
+    # Stokes 1
+    bcs1_1 = fd.DirichletBC(W.sub(0), noslip, WALLS)
+    bcs1_2 = fd.DirichletBC(W.sub(0), inflow1, INLET1)
+    bcs1_3 = fd.DirichletBC(W.sub(1), fd.Constant(0.0), OUTLET1)
+    bcs1_4 = fd.DirichletBC(W.sub(0), noslip, INLET2)
+    bcs1_5 = fd.DirichletBC(W.sub(0), noslip, OUTLET2)
+    bcs1 = [bcs1_1, bcs1_2, bcs1_3, bcs1_4, bcs1_5]
 
-LT_bnd = (
-    alpha / h * ks * tin1 * w * ds(INLET1)
-    + alpha / h * ks * tin2 * w * ds(INLET2)
-    - tin1 * ks * dot(grad(w), n) * ds(INLET1)
-    - tin2 * ks * dot(grad(w), n) * ds(INLET2)
-)
-eT = aT - LT_bnd
+    # Stokes 2
+    bcs2_1 = fd.DirichletBC(W.sub(0), noslip, WALLS)
+    bcs2_2 = fd.DirichletBC(W.sub(0), inflow2, INLET2)
+    bcs2_3 = fd.DirichletBC(W.sub(1), fd.Constant(0.0), OUTLET2)
+    bcs2_4 = fd.DirichletBC(W.sub(0), noslip, INLET1)
+    bcs2_5 = fd.DirichletBC(W.sub(0), noslip, OUTLET1)
+    bcs2 = [bcs2_1, bcs2_2, bcs2_3, bcs2_4, bcs2_5]
 
-problem = NonlinearVariationalProblem(eT, t)
-solver_temp = NonlinearVariationalSolver(
-    problem, solver_parameters=temperature_parameters
-)
-solver_temp.solve()
+    # Forward problems
+    U1, U2 = fd.Function(W), fd.Function(W)
+    L = inner(fd.Constant((0.0, 0.0, 0.0)), V) * dx
+    problem = fd.LinearVariationalProblem(
+        stokes(-phi, INMOUTH2, OUTMOUTH2), L, U1, bcs=bcs1
+    )
+    solver_stokes1 = fd.LinearVariationalSolver(
+        problem, solver_parameters=stokes_parameters
+    )
+    solver_stokes1.solve()
+    problem = fd.LinearVariationalProblem(
+        stokes(phi, INMOUTH1, OUTMOUTH1), L, U2, bcs=bcs2
+    )
+    solver_stokes2 = fd.LinearVariationalSolver(
+        problem, solver_parameters=stokes_parameters
+    )
+    solver_stokes2.solve()
 
-power_drop = 1e-2
-Power1 = assemble(p1 / power_drop * ds(INLET1))
-Power2 = assemble(p2 / power_drop * ds(INLET2))
-scale_factor = 4e-4
-Jform = assemble(Constant(-scale_factor * cp_value) * inner(t * u1, n) * ds(OUTLET1))
+    # Convection difussion equation
+    ks = fd.Constant(1e0)
+    cp_value = 5.0e5
+    cp = fd.Constant(cp_value)
+    T = fd.FunctionSpace(mesh, "DG", 1)
+    t = fd.Function(T, name="Temperature")
+    w = fd.TestFunction(T)
+
+    # Mesh-related functions
+    n = fd.FacetNormal(mesh)
+    h = fd.CellDiameter(mesh)
+    u1, p1 = fd.split(U1)
+    u2, p2 = fd.split(U2)
+
+    def upwind(u):
+        return (dot(u, n) + abs(dot(u, n))) / 2.0
+
+    u1n = upwind(u1)
+    u2n = upwind(u2)
+
+    # Penalty term
+    alpha = fd.Constant(500.0)
+    # Bilinear form
+    a_int = dot(grad(w), ks * grad(t) - cp * (u1 + u2) * t) * dx
+
+    a_fac = (
+        fd.Constant(-1.0) * ks * dot(avg(grad(w)), jump(t, n)) * dS
+        + fd.Constant(-1.0) * ks * dot(jump(w, n), avg(grad(t))) * dS
+        + ks("+") * (alpha("+") / avg(h)) * dot(jump(w, n), jump(t, n)) * dS
+    )
+
+    a_vel = (
+        dot(
+            jump(w),
+            cp * (u1n("+") + u2n("+")) * t("+") - cp * (u1n("-") + u2n("-")) * t("-"),
+        )
+        * dS
+        + dot(w, cp * (u1n + u2n) * t) * ds
+    )
+
+    a_bnd = (
+        dot(w, cp * dot(u1 + u2, n) * t) * (ds(INLET1) + ds(INLET2))
+        + w * t * (ds(INLET1) + ds(INLET2))
+        - w * tin1 * ds(INLET1)
+        - w * tin2 * ds(INLET2)
+        + alpha / h * ks * w * t * (ds(INLET1) + ds(INLET2))
+        - ks * dot(grad(w), t * n) * (ds(INLET1) + ds(INLET2))
+        - ks * dot(grad(t), w * n) * (ds(INLET1) + ds(INLET2))
+    )
+
+    aT = a_int + a_fac + a_vel + a_bnd
+
+    LT_bnd = (
+        alpha / h * ks * tin1 * w * ds(INLET1)
+        + alpha / h * ks * tin2 * w * ds(INLET2)
+        - tin1 * ks * dot(grad(w), n) * ds(INLET1)
+        - tin2 * ks * dot(grad(w), n) * ds(INLET2)
+    )
+    eT = aT - LT_bnd
+
+    problem = fd.NonlinearVariationalProblem(eT, t)
+    solver_temp = fd.NonlinearVariationalSolver(
+        problem, solver_parameters=temperature_parameters
+    )
+    solver_temp.solve()
+
+    # Cost function: Flux at the cold outlet
+    scale_factor = 4e-4
+    Jform = fd.assemble(
+        fd.Constant(-scale_factor * cp_value) * inner(t * u1, n) * ds(OUTLET1)
+    )
+    # Constraints: Pressure drop on each fluid
+    power_drop = 1e-2
+    Power1 = fd.assemble(p1 / power_drop * ds(INLET1))
+    Power2 = fd.assemble(p2 / power_drop * ds(INLET2))
+
+    phi_pvd = fd.File("phi_evolution.pvd")
+
+    def deriv_cb(phi):
+        with stop_annotating():
+            phi_pvd.write(phi[0])
+
+    c = fda.Control(s)
+
+    # Reduced Functionals
+    Jhat = LevelSetFunctional(Jform, c, phi, derivative_cb_pre=deriv_cb)
+    P1hat = LevelSetFunctional(Power1, c, phi)
+    P1control = fda.Control(Power1)
+
+    P2hat = LevelSetFunctional(Power2, c, phi)
+    P2control = fda.Control(Power2)
+
+    Jhat_v = Jhat(phi)
+    print("Initial cost function value {:.5f}".format(Jhat_v), flush=True)
+    print("Power drop 1 {:.5f}".format(Power1), flush=True)
+    print("Power drop 2 {:.5f}".format(Power2), flush=True)
+
+    beta_param = 0.08
+    # Regularize the shape derivatives only in the domain marked with 0
+    reg_solver = RegularizationSolver(
+        S, mesh, beta=beta_param, gamma=1e5, dx=dx, design_domain=0
+    )
+
+    # Solver to ensure the level set is a distance function
+    reinit_solver = ReinitSolverDG(n_steps=20, dt=1e-3)
+    hmin = 0.001
+    # Hamilton-Jacobi equation to advect the level set
+    hj_solver = HJLocalDG(hmin=hmin)
+    tol = 1e-5
+    dt = 0.02
+    params = {
+        "alphaC": 1.0,
+        "debug": 5,
+        "alphaJ": 1.0,
+        "dt": dt,
+        "K": 1e-3,
+        "maxit": opts.n_iters,
+        "maxtrials": 5,
+        "itnormalisation": 10,
+        "tol_merit": 5e-3,  # new merit can be within 0.5% of the previous merit
+        # "normalize_tol" : -1,
+        "tol": tol,
+    }
+
+    # Optimization problem
+    problem = InfDimProblem(
+        Jhat,
+        reg_solver,
+        hj_solver,
+        reinit_solver,
+        ineqconstraints=[
+            Constraint(P1hat, 1.0, P1control),
+            Constraint(P2hat, 1.0, P2control),
+        ],
+        reinit_steps=5,
+    )
+    results = nlspace_solve_shape(problem, params)
 
 
-phi_pvd = File("phi_evolution.pvd")
-
-
-def deriv_cb(phi):
-    with stop_annotating():
-        phi_pvd.write(phi[0])
-
-
-c = Control(s)
-
-# Reduced Functionals
-Jhat = LevelSetFunctional(Jform, c, phi, derivative_cb_pre=deriv_cb)
-P1hat = LevelSetFunctional(Power1, c, phi)
-P1control = Control(Power1)
-
-P2hat = LevelSetFunctional(Power2, c, phi)
-P2control = Control(Power2)
-
-tape = get_working_tape()
-Jhat_v = Jhat(phi)
-print("Initial cost function value {:.5f}".format(Jhat_v), flush=True)
-print("Power drop 1 {:.5f}".format(Power1), flush=True)
-print("Power drop 2 {:.5f}".format(Power2), flush=True)
-
-
-velocity = Function(S)
-beta_param = 0.08
-reg_solver = RegularizationSolver(
-    S, mesh, beta=beta_param, gamma=1e5, dx=dx, design_domain=0
-)
-
-
-reinit_solver = ReinitSolverDG(n_steps=20, dt=1e-3)
-hmin = 0.001
-hj_solver = HJLocalDG(hmin=hmin)
-tol = 1e-5
-dt = 0.02
-params = {
-    "alphaC": 1.0,
-    "debug": 5,
-    "alphaJ": 1.0,
-    "dt": dt,
-    "K": 1e-3,
-    "maxit": opts.n_iters,
-    "maxtrials": 5,
-    "itnormalisation": 10,
-    "tol_merit": 5e-3,  # new merit can be within 0.5% of the previous merit
-    # "normalize_tol" : -1,
-    "tol": tol,
-}
-
-problem = InfDimProblem(
-    Jhat,
-    reg_solver,
-    hj_solver,
-    reinit_solver,
-    ineqconstraints=[
-        Constraint(P1hat, 1.0, P1control),
-        Constraint(P2hat, 1.0, P2control),
-    ],
-    reinit_steps=5,
-)
-results = nlspace_solve_shape(problem, params)
+if __name__ == "__main__":
+    heat_exchanger_optimization()
