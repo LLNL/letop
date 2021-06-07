3b88d2fd94d94266493bfbaf9059274438a646c2
Miguel <salazardetro1@llnl.gov>
2021-06-07 13:14:06 -0700

    several improvements: get rid of viscosity and lower bound for the stabilization term

--- lestofire/physics/advection_diffusion.py
+++ lestofire/physics/advection_diffusion.py
@@ -2,7 +2,12 @@ import firedrake as fd
 from firedrake import inner, dot, grad, div, dx, sqrt
 
 
-def AdvectionDiffusionSUPG(V: fd.FunctionSpace, theta, k, phi, phi_t):
+def AdvectionSUPG(
+    V: fd.FunctionSpace,
+    theta: fd.Function,
+    phi: fd.Function,
+    phi_t: fd.Function,
+):
     rho = fd.TestFunction(V)
     cell_type = V.ufl_domain().ufl_coordinate_element().cell().cellname()
     if cell_type in ["triangle", "tetrahedron"]:
@@ -11,15 +16,11 @@ def AdvectionDiffusionSUPG(V: fd.FunctionSpace, theta, k, phi, phi_t):
         h = fd.CellDiameter(V.ufl_domain())
     else:
         raise RuntimeError(f"Element {cell_type} not supported")
-    F = (
-        phi_t * rho * dx
-        + (inner(theta, grad(phi)) * rho + k * inner(grad(phi), grad(rho)))
-        * dx
-    )
+    F = phi_t * rho * dx + (inner(theta, grad(phi)) * rho) * dx
 
     # Strong form esidual
-    r = phi_t + dot(theta, grad(phi)) - k * div(grad(phi))
+    r = phi_t + dot(theta, grad(phi))
     # Add SUPG stabilisation terms
-    vnorm = sqrt(dot(theta, theta)) + 1e-8
+    vnorm = sqrt(dot(theta, theta)) + 1e-3
     F += (h / (2.0 * vnorm)) * dot(theta, grad(rho)) * r * dx
     return F
